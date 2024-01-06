94e65af7f432f2e442be610bf39ac20e1a2e3131
gaetan.darrort <gaetan.darrort@epitech.eu>
2024-01-06 19:43:14 +0100

    Changed : minor improvement to middleware

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -6,7 +6,7 @@ import * as jwt from 'jsonwebtoken';
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
-    const token = req.cookies['accessToken']; // Your cookie name
+    const token = req.cookies['accessToken'];
 
     if (!token) {
       return res.status(401).json({ message: 'Access token is missing' });
@@ -25,20 +25,31 @@ export class AuthMiddleware implements NestMiddleware {
         const newTokenResponse = await axios.post(`${process.env.SUPABASE_API_URL}/auth/v1/token`, {
           refresh_token: refreshToken,
           grant_type: 'refresh_token',
+          apikey: process.env.SUPABASE_ANON_KEY,
         });
 
         const newAccessToken = newTokenResponse.data.access_token;
         if (newAccessToken) {
           res.cookie('accessToken', newAccessToken, { httpOnly: true });
           req.cookies['accessToken'] = newAccessToken;
+          res.cookie('refreshToken', newTokenResponse.data.refresh_token, { httpOnly: true });
+          req.cookies['refreshToken'] = newTokenResponse.data.refresh_token;
           next();
         } else {
-          return res.status(401).json({ message: 'Unable to refresh access token' });
+          throw new Error('Supabase did not return a new access token.');
         }
       } else {
         next();
       }
     } catch (error) {
+      if (axios.isAxiosError(error)) {
+        const response = error.response;
+        if (response && response.data) {
+          console.error('Supabase Error:', response.data);
+          return res.status(response.status).json({ message: response.data.message });
+        }
+      }
+
       console.error('Error in AuthMiddleware:', error.message || error);
       return res.status(500).json({ message: 'Internal Server Error' });
     }
