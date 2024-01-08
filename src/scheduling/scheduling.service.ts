555eefe75ff3687dbd7e67d2f0eddf84cfd377d8
gaetan.darrort <gaetan.darrort@epitech.eu>
2024-01-08 20:30:16 +0100

    Added : scheduling service that will trigger reation if an action is triggered

---
+++ src/scheduling/scheduling.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { SupabaseClient, createClient } from '@supabase/supabase-js';
+
+@Injectable()
+export class TasksService {
+  private readonly logger = new Logger(TasksService.name);
+  private supabaseAdmin: SupabaseClient;
+
+  constructor() {
+    // Initialisez Supabase avec la clé de service
+    this.supabaseAdmin = createClient(
+      process.env.SUPABASE_API_URL,
+      process.env.SUPABASE_SERVICE_ROLE_KEY,
+    );
+  }
+
+  @Cron(CronExpression.EVERY_10_SECONDS)
+  async handleCron() {
+    this.logger.debug('Called every 10 seconds');
+  }
+}
